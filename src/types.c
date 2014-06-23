0fd70471b6479ea2fcd5d17123be0988bbbbe7de
Mathias Buus <mathiasbuus@gmail.com>
2014-06-23 20:52:07 +0200

    c support for keeping aspect ratio

--- src/types.c
+++ src/types.c
@@ -8,11 +8,19 @@ int type_echo (MagickWand *wand, void *o) {
 int type_scale (MagickWand *wand, void *o) {
   scale_t *opts = (scale_t*) o;
 
+  if (opts->keep_ratio == 0) return MagickScaleImage(wand, opts->width, opts->height);
+
   unsigned long wid = MagickGetImageWidth(wand);
   unsigned long hei = MagickGetImageHeight(wand);
-  double ratio = (double)hei / (double)wid;
+  double ratio = (double)wid / (double)hei;
 
-  return MagickScaleImage(wand, opts->width, opts->height);
+  uint32_t new_wid = MIN(opts->width, wid);
+  uint32_t new_hei = MIN(opts->height, hei);
+
+  if (ratio > 1) new_hei = (double)new_wid / ratio;
+  else new_wid = (double)new_hei * ratio;
+
+  return MagickScaleImage(wand, new_wid, new_hei);
 }
 
 type_fn *type_to_function (int type) {
--- src/types.h
+++ src/types.h
@@ -4,9 +4,12 @@
 #include <wand/magick_wand.h>
 #include <stdint.h>
 
+#define MIN(x,y) (x < y ? x : y)
+
 typedef struct {
   uint32_t width;
   uint32_t height;
+  unsigned char keep_ratio;
 } scale_t;
 
 typedef int type_fn(MagickWand *wand, void *opts);
