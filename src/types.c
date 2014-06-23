3c9d80a4687024fd7d5596b5eabfc1d74b60b191
Mathias Buus <mathiasbuus@gmail.com>
2014-06-23 20:33:27 +0200

    renamed functions

--- src/types.c
+++ src/types.c
@@ -1,19 +1,24 @@
 #include <wand/magick_wand.h>
 #include "types.h"
 
-int on_echo (MagickWand *wand, void *o) {
+int type_echo (MagickWand *wand, void *o) {
   return MagickPass;
 }
 
-int on_scale (MagickWand *wand, void *o) {
+int type_scale (MagickWand *wand, void *o) {
   scale_t *opts = (scale_t*) o;
+
+  unsigned long wid = MagickGetImageWidth(wand);
+  unsigned long hei = MagickGetImageHeight(wand);
+  double ratio = (double)hei / (double)wid;
+
   return MagickScaleImage(wand, opts->width, opts->height);
 }
 
 type_fn *type_to_function (int type) {
   switch (type) {
-    case 0:  return &on_echo;
-    case 1:  return &on_scale;
+    case 0:  return &type_echo;
+    case 1:  return &type_scale;
     default: return NULL;
   }
 }
